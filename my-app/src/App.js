fa13f84d745ada1623fd6eb19921e37a99d50db6
andreadsw <andrea.dossantosw@gmail.com>
2017-03-30 17:58:17 +0100

    Adding library react-google-maps library, map and places components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Map from '../src/components/Map/Map'
+import Places from '../src/components/Places/Places'
 
 class App extends Component {
   render() {
+
+    const location = {
+      lat: 51.528308,
+      lng: -0.3817961
+    }
+
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +21,10 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div style={{width:600, height:600 }}>
+        <Map center={location} />
+        </div>
+        <Places />
       </div>
     );
   }
---
+++ my-app/src/components/Places/Places.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Places extends Component {
+    render() {
+        return(
+            <div>This will display my Places</div>
+        )       
+    }
+}
+
+export default Places
